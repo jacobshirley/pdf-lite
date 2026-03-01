d5fb0e3f792cd9d470ac00bd59668533bdacbbf3
jacobshirley <jakeshirley2@gmail.com>
2026-03-01 10:08:10 +0000

    feat: remove PdfAnnotationWriter class and its exports

--- packages/pdf-lite/src/annotations/index.ts
+++ packages/pdf-lite/src/annotations/index.ts
@@ -4,4 +4,3 @@ export {
 } from './pdf-annotation.js'
 export { PdfAnnotationFlags } from './pdf-annotation-flags.js'
 export { PdfWidgetAnnotation } from './pdf-widget-annotation.js'
-export { PdfAnnotationWriter } from './pdf-annotation-writer.js'

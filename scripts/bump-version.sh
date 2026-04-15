21f09b2037c4974f31016d9e826f5c6429d7803f
Jacob ‘Jake’ Shirley <jakeshirley2@gmail.com>
2026-04-15 17:13:27 +0100

    fix: correct flag usage for version bump in bump-version.sh

--- scripts/bump-version.sh
+++ scripts/bump-version.sh
@@ -8,10 +8,10 @@ set -e
 
 if [ -n "$1" ]; then
   # Bump version in root package.json
-  pnpm version --no-git-tag-version $@
+  pnpm version --git-tag-version=false $@
 
   # Bump version in all workspaces
-  pnpm -r exec pnpm version --no-git-tag-version $@
+  pnpm -r exec pnpm version --git-tag-version=false $@
 
   # Get the new version from root package.json
   NEW_VERSION=$(node -p "require('./package.json').version")
